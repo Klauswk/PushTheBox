88d21426db5930996287e008cec2edaa90c8ae75
Klaus Klein <klauswk1@hotmail.com>
2017-06-26 21:23:11 -0300

    Fix android problem

--- core/src/com/github/klauswk/object/Box.java
+++ core/src/com/github/klauswk/object/Box.java
@@ -4,98 +4,108 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.github.klauswk.drawable.Drawable;
 import com.github.klauswk.drawable.Moveable;
 import com.github.klauswk.player.Colliable;
 import com.github.klauswk.player.Direction;
 import com.github.klauswk.player.MapDetection;
+import java.util.Random;
 
-public class Box implements Moveable<Vector2> , Colliable {
+public class Box implements Moveable<Vector2>, Colliable {
 
-	private Vector2 pos;
-	private Texture texture;
-	private Rectangle rectangle;
-	private MapDetection mapDetection;
+    private Vector2 pos;
+    private Texture texture;
+    private Rectangle rectangle;
+    private MapDetection mapDetection;
 
-	public Box(int x, int y) {
-		this(new Vector2(x, y));
-	}
+    public Box(int x, int y) {
+        this(new Vector2(x, y));
+    }
 
-	public Box(Vector2 position) {
-		texture = new Texture("box.png");
+    public Box(Vector2 position) {
+        texture = new Texture("box.png");
+        System.out.println("x: " + position.x);
+        System.out.println("y: " + position.y);
+        position.x = reposite(position.x);
+        position.y = reposite(position.y);
+        System.out.println("xx: " + position.x);
+        System.out.println("yy: " + position.y);
 
-		while (position.x % 16 != 0) {
-			position.x--;
-		}
-		while (position.y % 16 != 0) {
-			position.y--;
-		}
-		
-		rectangle = new Rectangle(position.x,position.y,texture.getWidth(),texture.getHeight());
-		this.pos = position;
-	}
-	
-	public void setPosition(Vector2 position) {
-		this.pos = position;
-	}
-	
-	public Vector2 getPosition() {
-		return pos;
-	}
-	
-	private boolean overlaps;
-	
-	@Override
-	public boolean isColliding(Rectangle rectangle) {
-		overlaps = this.rectangle.overlaps(rectangle);
-		return overlaps;
-	}
-	
-	@Override
-	public void move(int posx, int posy) {
-		if (posx < 0) {
-			if (mapDetection.checkIfCanMove(Direction.LEFT)) {
-				this.pos.x += posx;
-			}
-		} else if (posx > 0) {
-			if (mapDetection.checkIfCanMove(Direction.RIGHT)) {
-				this.pos.x += posx;
-			}
-		}
-		if (posy < 0) {
-			if (mapDetection.checkIfCanMove(Direction.DOWN)) {
-				this.pos.y += posy;
-			}
-		} else if (posy > 0) {
-			if (mapDetection.checkIfCanMove(Direction.UP)) {
-				this.pos.y += posy;
-			}
-		}
-		rectangle.setX(this.pos.x);
-		rectangle.setY(this.pos.y);
-	}
+        rectangle = new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
+        this.pos = position;
+    }
 
-	@Override
-	public void render(SpriteBatch sb) {
-		sb.begin();
-		sb.draw(texture,pos.x,pos.y);
-		sb.end();
-	}
+    private float reposite(float position) {
+        while (position % 16 != 0) {
+            position--;
+            if (position == 0) {
+                position = 32;
+            }
+        }
 
-	@Override
-	public void update(float dt) {
-		
-	}
-	
-	public Rectangle getRectangle() {
-		return rectangle;
-	}
-	
-	public void setMapDetection(MapDetection mapDetection) {
-		this.mapDetection = mapDetection;
-	}
-	
-	public MapDetection getMapDetection() {
-		return mapDetection;
-	}
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.pos = position;
+    }
+
+    public Vector2 getPosition() {
+        return pos;
+    }
+
+    private boolean overlaps;
+
+    @Override
+    public boolean isColliding(Rectangle rectangle) {
+        overlaps = this.rectangle.overlaps(rectangle);
+        return overlaps;
+    }
+
+    @Override
+    public void move(int posx, int posy) {
+        if (posx < 0) {
+            if (mapDetection.checkIfCanMove(Direction.LEFT)) {
+                this.pos.x += posx;
+            }
+        } else if (posx > 0) {
+            if (mapDetection.checkIfCanMove(Direction.RIGHT)) {
+                this.pos.x += posx;
+            }
+        }
+        if (posy < 0) {
+            if (mapDetection.checkIfCanMove(Direction.DOWN)) {
+                this.pos.y += posy;
+            }
+        } else if (posy > 0) {
+            if (mapDetection.checkIfCanMove(Direction.UP)) {
+                this.pos.y += posy;
+            }
+        }
+        rectangle.setX(this.pos.x);
+        rectangle.setY(this.pos.y);
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(texture, pos.x, pos.y);
+        sb.end();
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setMapDetection(MapDetection mapDetection) {
+        this.mapDetection = mapDetection;
+    }
+
+    public MapDetection getMapDetection() {
+        return mapDetection;
+    }
 }
--- core/src/com/github/klauswk/object/BoxService.java
+++ core/src/com/github/klauswk/object/BoxService.java
@@ -4,53 +4,53 @@ import java.util.Random;
 
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
-import com.github.klauswk.player.MapDetection;
 import com.github.klauswk.util.NormalizeValue;
 import com.github.klauswk.util.NormalizeVector;
 import com.github.klauswk.util.Placeable;
 
-public class BoxService implements BoxFactory, Placeable<Box>{
+public class BoxService implements BoxFactory, Placeable<Box> {
 
-	private TiledMapTileLayer tiledMapTileLayer;
-	private Vector2 mapBounds;
-	private Random random;
-	private NormalizeValue<Vector2, Vector2> normalizeVector;
+    private TiledMapTileLayer tiledMapTileLayer;
+    private Vector2 mapBounds;
+    private Random random;
+    private NormalizeValue<Vector2, Vector2> normalizeVector;
 
-	public BoxService(TiledMapTileLayer tiledMapTileLayer) {
-		this.tiledMapTileLayer = tiledMapTileLayer;
+    public BoxService(TiledMapTileLayer tiledMapTileLayer) {
+        this.tiledMapTileLayer = tiledMapTileLayer;
 
-		mapBounds = new Vector2(256, 256);
-		random = new Random();
-		normalizeVector = new NormalizeVector();
-	}
+        mapBounds = new Vector2(256, 256);
+        random = new Random();
+        normalizeVector = new NormalizeVector();
+    }
 
-	@Override
-	public Box getInstance() {
-		Vector2 vec = normalizeVector
-				.normalize(new Vector2(random.nextInt((int) mapBounds.x), random.nextInt((int) mapBounds.y)));
-		Box box = new Box(vec);
-		checkIfCanBePlaced(box);
-		translate(box);
-		box.setMapDetection(new MapDetectionBox(this.tiledMapTileLayer, box.getPosition()));
-		return box;
-	}
+    @Override
+    public Box getInstance() {
+        Vector2 vec = normalizeVector
+                .normalize(new Vector2(random.nextInt((int) mapBounds.x), random.nextInt((int) mapBounds.y)));
+        Box box = new Box(vec);
+        checkIfCanBePlaced(box);
+        translate(box);
+        box.setMapDetection(new MapDetectionBox(this.tiledMapTileLayer, box.getPosition()));
+        return box;
+    }
 
-	private void translate(Box box) {
-		Vector2 vec = box.getPosition();
-		for (int i = 0; i < tiledMapTileLayer.getWidth() / 16; i++) {
-			for (int j = 0; j < tiledMapTileLayer.getHeight() / 16; j++) {
-				if (tiledMapTileLayer.getCell((int) vec.x / 16, (int) vec.y / 16).getTile().getId() == 83) {
-					vec.x = 16 * i;
-					vec.y = 16 * j;
-				} else {
-					return;
-				}
-			}
-		}
-	}
-	@Override
-	public boolean checkIfCanBePlaced(Box box) {
-		return tiledMapTileLayer.getCell((int) box.getPosition().x / 16, (int) box.getPosition().y / 16).getTile()
-				.getId() == 83;
-	}
+    private void translate(Box box) {
+        Vector2 vec = box.getPosition();
+        for (int i = 0; i < tiledMapTileLayer.getWidth() / 16; i++) {
+            for (int j = 0; j < tiledMapTileLayer.getHeight() / 16; j++) {
+                if (tiledMapTileLayer.getCell((int) vec.x / 16, (int) vec.y / 16).getTile().getId() == 83) {
+                    vec.x = 16 * i;
+                    vec.y = 16 * j;
+                } else {
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean checkIfCanBePlaced(Box box) {
+        return tiledMapTileLayer.getCell((int) box.getPosition().x / 16, (int) box.getPosition().y / 16).getTile()
+                .getId() == 83;
+    }
 }
